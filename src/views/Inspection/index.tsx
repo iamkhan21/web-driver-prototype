65882c15e9634bed1fc92b438df85dd895b9ce9e
Ivan Khanevich <mail@iamkhan.tech>
2022-04-02 23:16:43 +0300

    feat: add inspection flow

--- src/views/Inspection/index.tsx
+++ src/views/Inspection/index.tsx
@@ -1,3 +1,4 @@
+/*
 import React, { useEffect, useRef } from "react";
 import Button from "@mui/material/Button";
 
@@ -64,6 +65,134 @@ const Inspection = () => {
       </section>
 
       <canvas ref={canvas} className="hidden" />
+    </article>
+  );
+};
+
+export default Inspection;
+*/
+import React from "react";
+
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+type InspectionStep = {
+  label: string;
+  description?: string;
+  needPhoto?: boolean;
+  needSignature?: boolean;
+};
+
+const steps: InspectionStep[] = [
+  {
+    label: "Take a picture of the car from the front",
+    needPhoto: true,
+  },
+  {
+    label: "Take a picture of the car from the left side",
+    needPhoto: true,
+  },
+  {
+    label: "Take a picture of the car from behind",
+    needPhoto: true,
+  },
+  {
+    label: "Take a picture of the car from the right side",
+    needPhoto: true,
+  },
+  {
+    label: "Take pictures of damage",
+    description:
+      "Take pictures of damage, if any, to the car (scratches, dents, cracks and chips in the windows).",
+    needPhoto: false,
+  },
+  {
+    label: "Get a customer signature",
+    needSignature: true,
+  },
+  {
+    label: "Load the car on a tow truck",
+  },
+  {
+    label: "Make sure that the car is securely fastened",
+    description: `Check that all wheels of the vehicle are securely fastened with straps and that the straps are undamaged. 
+      Check that there are no parts on the vehicle that could be lost during towing.`,
+  },
+];
+
+const Inspection = () => {
+  // const photos = useRef<string[]>([]);
+  const [photos, setPhotos] = React.useState<string[]>([]);
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const jumpToStep = (step: number) => () => {
+    if (activeStep > step) setActiveStep(step);
+  };
+
+  const lastStep = steps.length - 1;
+
+  return (
+    <article className="content">
+      <section>
+        <p className="py-2">
+          Before you load the car on the tow truck,
+          <br />
+          you need to inspect the car to be picked up:
+        </p>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((step, index) => {
+            const isPhotoNeeded = "needPhoto" in step;
+            const cantMoveOn =
+              isPhotoNeeded && step.needPhoto && !photos[index];
+
+            return (
+              <Step key={step.label} onClick={jumpToStep(index)}>
+                <StepLabel>{step.label}</StepLabel>
+                <StepContent>
+                  {step.description && (
+                    <Typography>{step.description}</Typography>
+                  )}
+                  <div className="py-2 space-x-2">
+                    {isPhotoNeeded && (
+                      <Button
+                        variant="outlined"
+                        onClick={() => {
+                          photos[index] = "12" + index;
+                          setPhotos([...photos]);
+                        }}
+                      >
+                        Do photo
+                      </Button>
+                    )}
+                    <Button
+                      variant="contained"
+                      onClick={handleNext}
+                      disabled={cantMoveOn}
+                    >
+                      {index === lastStep ? "Finish" : "Next"}
+                    </Button>
+                    <Button disabled={index === 0} onClick={handleBack}>
+                      Back
+                    </Button>
+                  </div>
+                </StepContent>
+              </Step>
+            );
+          })}
+        </Stepper>
+      </section>
     </article>
   );
 };
